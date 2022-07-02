0871a5cb918db182d982e7c7ae09dee81bb0fbd8
Misty Strickland <107017184+Misty94@users.noreply.github.com>
2022-07-02 14:04:54 -0400

    initial commit

---
+++ day1.js
@@ -0,0 +1,20 @@
+// Code is read from the top down
+// Variable values may change depending on the order of the code
+
+
+// Declaring a variable (x) and assigning it a value (3)
+var x = 3
+
+// Declaring a variable (y) and assigning it a value ("Hello There")
+var y = "Hello There"
+
+console.log("***************Console Log x*******************")
+
+var z = 5
+
+var sum = x + z
+
+// Console Log?
+console.log("Hello World")
+
+console.log("***********Test Case Line 47 test.js****************")
